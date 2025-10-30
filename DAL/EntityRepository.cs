d64d760412e2638b1b04dc4f67ec71fbe8bea6ff
Shirokiy <Puzikok@gmail.com>
2025-10-30 19:21:12 +0700

    Rename

--- DAL/EntityRepository.cs
+++ DAL/EntityRepository.cs
@@ -10,41 +10,41 @@ namespace DataAccessLayer
 {
     public class EntityRepository<T> : IRepository<T> where T : class, IDomainObject
     {
-        private readonly AppDBContext _context;
-        private readonly DbSet<T> _dbSet;
+        private readonly AppDBContext dataBaseContext;
+        private readonly DbSet<T> dbSet;
 
         public EntityRepository(AppDBContext context)
         {
-            _context = context;
-            _dbSet = _context.Set<T>();
+            dataBaseContext = context;
+            dbSet = dataBaseContext.Set<T>();
         }
         public void Add(T entity)
         {
-            _dbSet.Add(entity);
+            dbSet.Add(entity);
         }
         public void Delete(int id)
         {
-            var entity = _dbSet.Find(id);
+            var entity = dbSet.Find(id);
             if (entity != null)
-                _dbSet.Remove(entity);
+                dbSet.Remove(entity);
         }
         public T? ReadById(int id)
         {
-            return _dbSet.Find(id);
+            return dbSet.Find(id);
         }
         public List<T> ReadAll()
         { 
-            return _dbSet.ToList(); 
+            return dbSet.ToList(); 
         }
 
         public void Update(T entity)
         {
-            _dbSet.Attach(entity);
-            _context.Entry(entity).State = EntityState.Modified;
+            dbSet.Attach(entity);
+            dataBaseContext.Entry(entity).State = EntityState.Modified;
         }
         public void SaveChanges()
-        { 
-            _context.SaveChanges(); 
+        {
+            dataBaseContext.SaveChanges(); 
         }
 
 
